4b735b8522bdb8907b4e432df0d2da0dab3f026c
pountzas <nikos@pountzas.gr>
2021-04-06 01:18:29 +0300

    delete all start fresh

--- Gemfile
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
-
-# Specify your gem's dependencies in spaceCop.gemspec
-gemspec
-
-gem "rake", "~> 13.0"
--- lib/spaceCop.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "spaceCop/version"
-
-module SpaceCop
-  class Error < StandardError; end
-  # Your code goes here...
-end
