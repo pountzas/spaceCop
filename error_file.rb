391853177c5a996e4e3257c7b9620bf0ad402cc9
pountzas <nikos@pountzas.gr>
2021-04-06 19:50:14 +0300

    add methods to file for error testing

---
+++ error_file.rb
@@ -0,0 +1,25 @@
+class Player
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+def check_win(player_arr, winning_array)
+  control = false
+  for array in winning_array do
+    control = true if array.all? { |num| player_arr.include?(num) }
+  end
+  control
+end
+
+def design_board(slots)
+  "    ¤---¤---¤---¤
+    ¦ #{slots[0]} ¦ #{slots[1]} ¦ #{slots[2]} ¦
+    ¤---¤---¤---¤
+    ¦ #{slots[3]} ¦ #{slots[4]} ¦ #{slots[5]} ¦
+    ¤---¤---¤---¤
+    ¦ #{slots[6]} ¦ #{slots[7]} ¦ #{slots[8]} ¦
+    ¤---¤---¤---¤"
+end
