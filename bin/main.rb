b75031250afcfe69972afd3441f81f7bbcdd3c25
pountzas <nikos@pountzas.gr>
2021-04-07 03:33:30 +0300

    add colors to msg's and trailing

--- bin/main.rb
+++ bin/main.rb
@@ -2,8 +2,9 @@ require 'colorize'
 require_relative '../lib/file'
 
 file = File.new(ARGV.first)
-puts "File #{file.path}.. is been searched for errors."
-return 'No file detected to search for errors.' if file.path.nil?
+puts "File #{file.path.colorize(:blue)}.." + ' is been searched for errors.'.colorize(:yellow)
+no_file = 'No file detected to search for errors.'
+return no_file if file.path.nil?
 
 file.file_info
 file.last_line
--- lib/error_search.rb
+++ lib/error_search.rb
@@ -1,13 +1,19 @@
 module ErrorSearch
+  def trailing
+    msg = 'Last character in line has a space'.colorize(:red)
+    @lines.each_with_index do |line, x|
+      
+  end
+
   def line_length
-    msg = 'Line has to many characters'
+    msg = 'Line has to many characters'.colorize(:red)
     @lines.each_with_index do |line, x|
       line.length > 120 ? @error_list << "line:#{x + 1}:0 #{msg}" : false
     end
   end
 
   def blank_line
-    msg = 'empty line is missing'
+    msg = 'empty line found error'.colorize(:red)
     a = %w[def class module for if while unless until]
     @lines.each_with_index do |line, x|
       if a.include?(line.strip.split.first)
@@ -19,7 +25,7 @@ module ErrorSearch
   end
 
   def last_line
-    msg = 'Final empty line is missing'
+    msg = 'Final empty line is missing'.colorize(:red)
     @lines[-1].match?(/\n/) ? false : @error_list << "line:#{@total_lines + 1}:0 #{msg}"
   end
 end
